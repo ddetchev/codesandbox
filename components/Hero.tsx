67dd79a885b054da609037a6d3e9f0e25e237e3b
Daniel Detchev <white_daniell@berkeley.edu>
2023-12-26 20:12:39 -0800

    testing this

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -40,7 +40,7 @@ export function Hero() {
       <section className="flex justify-around items-center mt-5">
         <article className="">
           <h3 className="font-space font-bold">Agustin N. R. Ramirez</h3>
-          <h5 className="font-space">Frontend Developer</h5>
+          <h5 className="font-space">Frontend Developerrr</h5>
         </article>
         <video
           height={512}
